a3f3a88fc55ad926c9239cb7a8979165852a7e0c
PRAVESH TIWARI <pravesht252@gmail.com>
2026-02-20 21:58:38 +0530

    Update backend

--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,88 @@
+// const express = require('express');
+// const db = require('./config/db');
+// const router = require('./controllers/authController');
+// const userData = require('./routers/userData');
+// const chatData = require('./routers/chatData');
+// let app = express();
+// const cors = require('cors');
+// const http = require('http');
+// const {Server} = require('socket.io');
+
+
+// const initSocket = require('./sockets/socket');
+
+// db();
+
+// app.use(cors({
+//     origin:["http://localhost:5173","https://dapper-tapioca-de6dbd.netlify.app"]
+// }));
+// app.use(cors());
+// app.use(express.json())
+// app.use('/api',router);
+// app.use('/userdata', userData);
+// app.use('/chatdata', chatData);
+// // console.log(router);
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//     cors:{
+//         origin:["http://localhost:5173","https://dapper-tapioca-de6dbd.netlify.app"],
+//         methods:["GET","POST"]
+//     }
+// })
+
+
+// initSocket(io);
+
+// server.listen(4000, ()=>{
+//     console.log("Server running on port no 4000");
+// })
+
+
+
 const express = require('express');
 const db = require('./config/db');
 const router = require('./controllers/authController');
 const userData = require('./routers/userData');
 const chatData = require('./routers/chatData');
-let app = express();
 const cors = require('cors');
 const http = require('http');
-const {Server} = require('socket.io');
-
+const { Server } = require('socket.io');
 
 const initSocket = require('./sockets/socket');
 
+const app = express();
 db();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dapper-tapioca-de6dbd.netlify.app"
+];
+
 app.use(cors({
-    origin:["http://localhost:5173","https://dapper-tapioca-de6dbd.netlify.app"]
+  origin: allowedOrigins,
+  credentials: true
 }));
-app.use(cors());
-app.use(express.json())
-app.use('/api',router);
+
+app.use(express.json());
+
+app.use('/api', router);
 app.use('/userdata', userData);
 app.use('/chatdata', chatData);
-// console.log(router);
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors:{
-        origin:["http://localhost:5173","https://dapper-tapioca-de6dbd.netlify.app"],
-        methods:["GET","POST"]
-    }
-})
-
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 initSocket(io);
 
-server.listen(4000, ()=>{
-    console.log("Server running on port no 4000");
-})
+server.listen(4000, () => {
+  console.log("Server running on port no 4000");
+});
--- Server/sockets/socket.js
+++ Server/sockets/socket.js
@@ -1,35 +1,93 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+
+// const onlineUsers = {};
+
+// module.exports = (io) =>{
+//     io.use((socket, next)=>{
+//         const token = socket.handshake.auth.token;
+//         try{
+//             const user = jwt.verify(token, "PRIVATESTRING");
+//             socket.user = user;
+//             next();
+//         }
+//         catch{
+//             next(new Error("Unauthorized"));
+//         }
+//     })
+//     io.on("connection", (socket)=>{
+//         onlineUsers[socket.user._id] = socket.id;
+//         io.emit("online_users", Object.keys(onlineUsers));
+//         socket.on("private_message", ({to, message})=>{
+//             const targetId = onlineUsers[to];
+//             if(targetId){
+//                 io.to(targetId).emit("receive_private_message", {
+//                     from: socket.user._id,
+//                     message
+//                 })
+//             }
+//         })
+//         socket.on("disconnect", ()=>{
+//             delete onlineUsers[socket.user.userName];
+//             io.emit("online_users", Object.keys(onlineUsers));
+//         })
+//     })
+// }
+
+
+const jwt = require("jsonwebtoken");
 
 const onlineUsers = {};
 
-module.exports = (io) =>{
-    io.use((socket, next)=>{
-        const token = socket.handshake.auth.token;
-        try{
-            const user = jwt.verify(token, "PRIVATESTRING");
-            socket.user = user;
-            next();
-        }
-        catch{
-            next(new Error("Unauthorized"));
-        }
-    })
-    io.on("connection", (socket)=>{
-        onlineUsers[socket.user._id] = socket.id;
-        io.emit("online_users", Object.keys(onlineUsers));
-        socket.on("private_message", ({to, message})=>{
-            const targetId = onlineUsers[to];
-            if(targetId){
-                io.to(targetId).emit("receive_private_message", {
-                    from: socket.user._id,
-                    message
-                })
-            }
-        })
-        socket.on("disconnect", ()=>{
-            delete onlineUsers[socket.user.userName];
-            io.emit("online_users", Object.keys(onlineUsers));
-        })
-    })
-}
+module.exports = (io) => {
 
+  io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+
+    if (!token) {
+      return next(new Error("Unauthorized - No Token"));
+    }
+
+    try {
+      const user = jwt.verify(token, "PRIVATESTRING");
+      socket.user = user;
+      next();
+    } catch (err) {
+      next(new Error("Unauthorized - Invalid Token"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+
+    console.log("Connected:", socket.user._id);
+
+    // ✅ Store user correctly
+    onlineUsers[socket.user._id] = socket.id;
+
+    io.emit("online_users", Object.keys(onlineUsers));
+
+    socket.on("private_message", ({ to, message }) => {
+
+      const targetSocketId = onlineUsers[to];
+
+      if (targetSocketId) {
+        io.to(targetSocketId).emit("receive_private_message", {
+          from: socket.user._id,
+          to: to,              // ✅ IMPORTANT FIX
+          message,
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+
+      console.log("Disconnected:", socket.user._id);
+
+      // ✅ Correct delete
+      delete onlineUsers[socket.user._id];
+
+      io.emit("online_users", Object.keys(onlineUsers));
+    });
+
+  });
+
+};
